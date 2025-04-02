f9e66c09f3e60000825ccb41e01a5f5694421ad0
27stephentran <hazazoro@gmail.com>
2025-04-02 15:05:50 +0700

    Update Render and docker

--- server.py
+++ server.py
@@ -5,17 +5,26 @@ import pickle
 app = Flask(__name__)
 
 
-model = pickle.load(open('machinelearning_model.pkl', 'rb'))
+try:
+    model = pickle.load(open('machinelearning_model.pkl', 'rb'))
+except Exception as e:
+    print(f"Error loading model: {e}")
+    exit(1)
 
 @app.route("/predict/", methods=["POST"])
 def predict():
     data = request.get_json()
     features = data.get("features")
     
-    if features:
-        prediction = model.predict([features]) 
-        return jsonify({"predicted_class": prediction[0]})  
-    return jsonify({"error": "Dữ liệu không hợp lệ"}), 400
+    if not features:
+        return jsonify({"error": "Dữ liệu không hợp lệ, 'features' không được cung cấp"}), 400
+    
+    try:
+        features = [float(x) for x in features]
+        prediction = model.predict([features])
+        return jsonify({"predicted_class": prediction[0]})
+    except Exception as e:
+        return jsonify({"error": f"Xảy ra lỗi khi xử lý dữ liệu đầu vào: {str(e)}"}), 500
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  
